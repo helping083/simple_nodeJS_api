c066b7cd4cfae496ee0e0697c324167583dba048
helping083 <helping083@gmail.com>
2019-05-12 17:35:15 +0300

    bascic routes and mongoose

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const app = express();
 const moongose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
+
+app.use(bodyParser.json());
+
+const postsRoutes = require('./routes/posts');
+
+app.use('/posts', postsRoutes);
 
 //ROUTES
 app.get('/', (req,res)=>{
     res.send('hello world')
 });
 
-app.get('/posts', (req,res)=>{
-    res.send('smth went wrong');
-})
-
 //connect to db
 moongose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },
     ()=> {
         console.log('connectesss')
     }
-)
-
+);
 
 //server
 app.listen(3000);
