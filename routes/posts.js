6ea15a20f5a9323b8626a3ca8cfd20431bfc5803
helping083 <helping083@gmail.com>
2019-05-13 19:44:31 +0300

    patch a post

--- routes/posts.js
+++ routes/posts.js
@@ -34,13 +34,30 @@ router.get('/:postId', async (req,res)=>{
         res.json(err);
     }
 });
-
+// delete a specific post
 router.delete('/:postId', async (req,res)=>{
     try{
-        Post.remove({_id:req.params.postId});
+       const removedPost = await Post.remove({_id:req.params.postId});
+       res.json(removedPost);
     }catch(err) {
         res.json(err);
     }
 });
-
+//update a specific post
+router.patch('/:postId', async (req,res)=>{
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId},
+            {$set: 
+                {
+                    title: req.body.title, 
+                    description:req.body.description
+                    }
+            }
+        );
+        res.json(updatedPost);
+    }catch(err) {
+        res.json(err);
+    }
+});
 module.exports = router;
