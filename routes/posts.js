946bbfb3b97576bec9645be91cf46f626b92d3b0
helping083 <helping083@gmail.com>
2019-05-13 19:22:43 +0300

    d

--- routes/posts.js
+++ routes/posts.js
@@ -2,19 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req,res)=>{
-    res.send('in router');
+//get all posts
+router.get('/', async (req,res)=>{
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err) {
+        res.json(err);
+    }
 });
-
-router.post('/', (req,res)=>{
+//create a post
+router.post('/', async (req,res)=>{
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
+    try {
+        const newPost = await post.save();
+        res.json(newPost);
+    }catch(err) {
+        res.json(err);
+    }
+});
 
-    post.save().then((item)=>{
-        res.json(item)
-    }).catch((err)=>{res.json(err)})
+//get a specific post
+router.get('/:postId', async (req,res)=>{
+    try{
+        let singlePost =  await Post.findById(req.params.postId);
+        res.json(singlePost);
+    }catch(err){
+        res.json(err);
+    }
 });
 
 module.exports = router;
