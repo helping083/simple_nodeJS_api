7a63802f0dfb8935fe2c16c79cbd641e383da6d5
helping083 <helping083@gmail.com>
2019-07-16 22:36:24 +0300

    final

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const postsRoute = require('./routes/posts');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 
 //routes
@@ -16,7 +16,7 @@ app.use('/posts', postsRoute);
 
 //connect to db
 mongoose.connect('mongodb+srv://helping083:e2103443@cluster0-8mjxj.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true }, ()=>{
-    console.log('connected to db')
+    console.log('cosssnnected to db')
 });
 
 app.listen(3000);
